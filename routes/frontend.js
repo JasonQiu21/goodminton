7c93f553aafea6b391cbf387259bb81fadb7b6d1
Britney Yang <byang.careers@gmail.com>
2023-12-16 17:06:47 -0500

    added create event to nav bar

--- routes/frontend.js
+++ routes/frontend.js
@@ -5,8 +5,13 @@ const router = Router();
 router
     .route('/createEvent')
     .get(async(req, res) => {
+        let role;
+        if (req.session.user) {
+            role = req.session.role === "admin";
+
+        }
         try {
-            res.render('createEvent');
+            res.render('createEvent', {isAdmin: role});
         }catch(e) {
             res.render("error");
         }
