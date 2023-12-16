5b70e45eb22d888ebb81c3e190008c269cf3c62e
Britney Yang <byang.careers@gmail.com>
2023-12-15 23:12:31 -0500

    more changes :D

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ import express from "express";
 const app = express();
 import configRoutes from "./routes/index.js";
 import exphbs from "express-handlebars";
+import { fileURLToPath } from "url";
+import { dirname } from 'path';
+import path from 'path';
 
 /*
 TODO:
 - Build out routes
 */
-
-app.use("/public", express.static("public"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + "/public");
+app.use("/public", staticDir);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 configRoutes(app);
 
--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -1,6 +1,12 @@
-document.addEventListener("submit", (event) => {
+const isNonEmptyString = (str) => typeof str === 'string' && !!(str.trim());
+const validateFormSubmission = () => {
     let eventNameInput = document.getElementById('eventNameInput').value.trim();
     let eventDateInput = document.getElementById('eventDateInput').value.trim();
     let eventTypeInput = document.getElementById('eventTypeInput').value.trim();
-    
+    if (!isNonEmptyString(eventNameInput)) return "Invalid event name";
+    if (!isNonEmptyString(eventDateInput)) return "Invalid event date";
+    if (!isNonEmptyString(eventTypeInput)) return "Invalid event type";
+}
+document.addEventListener("submit", (event) => {
+
 })
