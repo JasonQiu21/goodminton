3a9eb3a54f0684681e85402ea3fa443a60c2af6b
Jason Qiu <jqiu21@protonmail.com>
2023-12-17 15:46:24 -0500

    Fixed single event view when not signed in

--- routes/frontend-events.js
+++ routes/frontend-events.js
@@ -37,13 +37,10 @@ router.route("/:id").get(async (req, res) => {
     const event = await getEvent(req.params.id)
     var playerReservations = [];
     const isLoggedIn = req.session?.player;
-    try {
+    if(req.session?.player){
       playerReservations = await getReservations(req.session?.player?._id);
     }
-    catch (e) {
-      console.log(e);
-    }
-    const inEvent = playerReservations.some((event) => event._id === req.params.id);
+    let inEvent = playerReservations.some((event) => event._id === req.params.id);
     var inEventTime = -1;
     if (inEvent) {
       inEventTime = playerReservations.filter((event) => event._id === req.params.id)[0].time;
@@ -74,7 +71,7 @@ router.route("/:id").get(async (req, res) => {
   } catch (e) {
     return res.render("error", {
       user: req.session?.player,
-      id: req.sesion?.player?._id,
+      id: req.session?.player?._id,
       error: e.error,
     });
   }
