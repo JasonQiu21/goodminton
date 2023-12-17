d56827db615d69f56b9015ba8ae66be546ea24ff
Aidan Haberman <ahaberma@stevens.edu>
2023-12-16 20:42:52 -0500

    Auto stash before merge of "frontend" and "origin/frontend"

--- routes/frontend-events.js
+++ routes/frontend-events.js
@@ -47,4 +47,31 @@ router.route("/:id").get(async (req, res) => {
   }
 });
 
+router.route("/:id").get(async (req, res) => {
+  try {
+    const event = await getEvent(req.params.id)
+    console.log(event);
+    const eventDate = new Date(event.date * 1000);
+    event.date = eventDate.toDateString();
+    event.time = eventDate.toTimeString();
+    for (let i = 0; i < event.reservations.length; i++) {
+      const reservationTime = new Date(event.reservations[i].time * 1000);
+      event.reservations[i].timeStamp = event.reservations[i].time;
+      event.reservations[i].date = reservationTime.toDateString();
+      event.reservations[i].time = reservationTime.toTimeString();
+      event.reservations[i].isFull = event.reservations[i].players.length===event.reservations[i].max;
+    }
+    event.user = req.session?.player;
+    event.id = req.session?.player?._id;
+    event.isPractice = event.eventType == "practice";
+    return res.render("event", event);
+  } catch (e) {
+    return res.render("error", {
+      user: req.session?.player,
+      id: req.sesion?.player?._id,
+      error: e.error,
+    });
+  }
+});
+
 export default router;
