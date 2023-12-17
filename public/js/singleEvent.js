8be6d3aca072e1abd0ffa3dbc43e695b2197bab4
Aidan Haberman <ahaberma@stevens.edu>
2023-12-16 21:13:52 -0500

    event handling almost complete

--- public/js/singleEvent.js
+++ public/js/singleEvent.js
@@ -1,12 +1,13 @@
-$("#join-button").click(function (e) {
+$(".join-button").click(function (e) {
     console.log("button pressed");
-    //e.preventDefault();
+    e.preventDefault();
     const eventId = window.location.pathname.split('/').at(-1);
-    const timeStamp = $("#join-button").attr("data-id")
+    var timeStamp = $(this).attr("data-id")
+    console.log(timeStamp);
     try {
-      isValidString($("#timeStamp").val());
+      timeStamp = isValidString(timeStamp);
       $.ajax({
-        url: `/api/reserve/${eventId}`,
+        url: `/api/events/reserve/${eventId}`,
         type: "POST",
         data: {
           time: timeStamp,
@@ -19,6 +20,20 @@ $("#join-button").click(function (e) {
         },
       });
     } catch (e) {
-      error(e.error);
+      console.log(e);
     }
   });
+
+const isValidString = (
+    input,
+    name = "String input",
+    allow_empty = false
+  ) => {
+    if (!input) throw { status: 400, error: `${name} must be provided.` };
+    if (typeof input !== "string")
+      throw { status: 400, error: `${name} is not a string.` };
+    if (!allow_empty && input.trim().length === 0)
+      throw { status: 400, error: `${name} is blank.` };
+  
+    return input.trim();
+};
