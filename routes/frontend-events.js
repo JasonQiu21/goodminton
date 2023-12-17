5a94bf3c0cc2b3e182f52f9a9e6dd1422782312a
YouLikeCats <thisisbryanchan@gmail.com>
2023-12-17 12:56:08 -0500

    Last push before merging with main

--- routes/frontend-events.js
+++ routes/frontend-events.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as typecheck from "../typecheck.js";
-import {getBracket} from "../data/events.js";
+import { getBracket } from "../data/events.js";
 const router = Router();
 
 router.route("/").get(async (req, res) => {
@@ -16,10 +16,10 @@ router.route("/:id/bracket").get(async (req, res) => {
   try {
     let id = typecheck.isValidId(req.params.id);
     let bracket = await getBracket(id);
-    return res.render("bracket", { user: req.session?.player , bracket: bracket});
+    return res.render("bracket", { user: req.session?.player, bracket: bracket });
   } catch (e) {
     return res.render("error", { user: req.session?.player, error: e.error });
-  } 
+  }
 });
 
 export default router;
