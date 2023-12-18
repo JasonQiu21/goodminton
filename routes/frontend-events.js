277ffb31acadb493802f6f9868572245e7bef019
Aidan Haberman <ahaberma@stevens.edu>
2023-12-17 19:33:19 -0500

    working frontend events js

--- routes/frontend-events.js
+++ routes/frontend-events.js
@@ -38,44 +38,44 @@ router.route("/:id").get(async (req, res) => {
   try {
     const event = await getEvent(req.params.id);
 
-    if(event?.tournamentType === "round robin"){
-      var playerId = req.session?.player?._id;
-      const loggedIn = req.session?.player;
-      // check for player in event
-      var inEvent = false;
-      event.reservations[0].players.forEach(player => {
-        if(player._id.toString() === playerId){
-          inEvent = true;
-        }
-      });
-      const timeStamp = event.reservations[0].time;
-
-      return res.render("roundrobin", { 
+    if (event?.tournamentType === "round robin") {
+      return res.render("roundrobin", {
         title: event.name,
         user: req.session?.player,
         id: req.session?.player?._id,
-        inTimeslot: inEvent,
-        timeStamp: timeStamp,
-        loggedIn: loggedIn,
-        isAdmin: req.session?.player?.role === "admin"
+        isAdmin: req.session?.player?.role === "admin",
       });
     }
 
-    else { 
-      var playerReservations = [];
-      const isLoggedIn = req.session?.player;
-      if (req.session?.player) {
-        playerReservations = await getReservations(req.session?.player?._id);
-      }
-      let inEvent = playerReservations.some(
+    var playerReservations = [];
+    const isLoggedIn = req.session?.player;
+    if (req.session?.player) {
+      playerReservations = await getReservations(req.session?.player?._id);
+    }
+    let inEvent = playerReservations.some(
+      (event) => event._id === req.params.id
+    );
+    var inEventTime = -1;
+    if (inEvent) {
+      inEventTime = playerReservations.filter(
         (event) => event._id === req.params.id
-      );
-      var inEventTime = -1;
-      if (inEvent) {
-        inEventTime = playerReservations.filter(
-          (event) => event._id === req.params.id
-        )[0].time;
-<<<<<<< Updated upstream
+      )[0].time;
+    }
+    if (!isLoggedIn) {
+      inEvent = true;
+    }
+    const eventDate = new Date(event.date * 1000);
+    event.date = eventDate.toDateString();
+    event.time = eventDate.toTimeString();
+    for (let i = 0; i < event.reservations.length; i++) {
+      const reservationTime = new Date(event.reservations[i].time * 1000);
+      event.reservations[i].timeStamp = event.reservations[i].time;
+      event.reservations[i].inEvent = inEvent;
+      if (event.reservations[i].time === inEventTime) {
+        // might need to multiply by 1000
+        event.reservations[i].inTimeslot = true;
+      } else {
+        event.reservations[i].inTimeslot = false;
       }
       event.reservations[i].date = reservationTime.toDateString();
       event.reservations[i].time = reservationTime.toTimeString();
@@ -136,48 +136,6 @@ router.route("/:id").get(async (req, res) => {
       error: e.error,
     });
   }
-=======
-      }
-      if (!isLoggedIn) {
-        inEvent = true;
-      }
-      const eventDate = new Date(event.date * 1000);
-      event.date = eventDate.toDateString();
-      event.time = eventDate.toTimeString();
-      for (let i = 0; i < event.reservations.length; i++) {
-        const reservationTime = new Date(event.reservations[i].time * 1000);
-        event.reservations[i].timeStamp = event.reservations[i].time;
-        event.reservations[i].inEvent = inEvent;
-        if (event.reservations[i].time === inEventTime) {
-          // might need to multiply by 1000
-          event.reservations[i].inTimeslot = true;
-        } else {
-          event.reservations[i].inTimeslot = false;
-        }
-        event.reservations[i].date = reservationTime.toDateString();
-        event.reservations[i].time = reservationTime.toTimeString();
-        event.reservations[i].isFull = event.reservations[i].players.length === event.reservations[i].max;
-      }
-      event.title = event.name;
-      event.user = req.session?.player;
-      event.id = req.session?.player?._id;
-      event.isPractice = event.eventType == "practice";
-      event.isSingleElim = event.eventType == "Single Elimination Tournament";
-      if (event.isPractice) {
-        return res.render("event", event);
-      } else if (event.isSingleElim) {
-        return res.render("bracket", event);
-      }
-    }
-    } catch (e) {
-      return res.render("error", {
-        title: "Error",
-        user: req.session?.player,
-        id: req.session?.player?._id,
-        error: e.error,
-      });
-    }
->>>>>>> Stashed changes
 });
 
 export default router;
