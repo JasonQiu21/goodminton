ef71dadd0d55c7aaf3f6ad5e814fb761b78e254f
yjiang0805 <115419442+yjiang0805@users.noreply.github.com>
2023-12-17 22:07:59 -0500

    Frontend event capacity

--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -1,3 +1,5 @@
+import { isValidNumber } from "../../typecheck";
+
 const isNonEmptyString = (str) => typeof str === "string" && !!str.trim();
 function isValidDate(dateString) {
   var regEx = /^\d{4}-\d{2}-\d{2}$/;
@@ -18,14 +20,18 @@ const validateFormSubmission = () => {
     .getElementById("tournamentTypeInput")
     .value.trim()
     .toLowerCase();
+  let eventCapInput = document.getElementById("eventCapInput");
 
   if (!isNonEmptyString(eventNameInput)) return "Invalid event name";
   if (!isNonEmptyString(eventDateInput)) return "Invalid event date";
   if (!isNonEmptyString(eventTypeInput)) return "Invalid event type";
   if (!isNonEmptyString(tournamentTypeInput)) return "Invalid tournament type";
+
   const validDate = isValidDate(eventDateInput);
+  const validNum = isValidNumber(eventCapInput);
   if (!validDate) return "Invalid event date";
   eventDateInput = new Date(eventDateInput).getTime() / 1000;
+  if (!validNum) return "Invalid event capacity";
   if (
     !["doubles tournament", "singles tournament", "practice"].includes(
       eventTypeInput.toLowerCase()
@@ -38,7 +44,13 @@ const validateFormSubmission = () => {
     )
   )
     return "Invalid tournament type";
-  return [eventNameInput, eventDateInput, eventTypeInput, tournamentTypeInput];
+  return [
+    eventNameInput,
+    eventDateInput,
+    eventTypeInput,
+    tournamentTypeInput,
+    eventCapInput,
+  ];
 };
 // document.addEventListener("submit", (event) => {
 //     let output = document.getElementById('output-error');
