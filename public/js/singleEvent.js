fe166aa3319815ba80a87564af7470feab8bb509
brrryry <thisisbryanchan@gmail.com>
2023-12-18 22:23:58 -0500

    Fixed bug where you didn't get an error message when joining a full even

--- public/js/singleEvent.js
+++ public/js/singleEvent.js
@@ -4,26 +4,29 @@ $(".join-button").click(function (e) {
     const eventId = window.location.pathname.split('/').at(-1);
     var timeStamp = $(this).data("id")
     console.log(timeStamp);
-    try {
-      $.ajax({
-        url: `/api/events/reserve/${eventId}`,
-        type: "POST",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify({
-          time: timeStamp,
-        }),
-        success: function (result) {
-          console.log(result);
-          location.reload(true);
-        //   $("#playerName").attr("placeholder", result.playerName);
-        //   $("#playerName").val("");
-        //   success("Player name updated!");
-        },
-      });
-    } catch (e) {
-      console.log(e);
-    }
+  
+    $.ajax({
+      url: `/api/events/reserve/${eventId}`,
+      type: "POST",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        time: timeStamp,
+      }),
+      success: function (result) {
+        $("#error").hide();
+        console.log(result);
+        location.reload(true);
+      //   $("#playerName").attr("placeholder", result.playerName);
+      //   $("#playerName").val("");
+      //   success("Player name updated!");
+      },
+      error: function(result) {
+        $("#error").removeAttr("hidden");
+        $("#error").text(result.responseJSON.error);
+      }
+    });
+  
 });
 
 $(".leave-button").click(function (e) {
@@ -41,28 +44,19 @@ $(".leave-button").click(function (e) {
         data: JSON.stringify({
         }),
         success: function (result) {
+          $("#error").hide();
           console.log(result);
           location.reload(true);
         //   $("#playerName").attr("placeholder", result.playerName);
         //   $("#playerName").val("");
         //   success("Player name updated!");
         },
+        error: function(result) {
+          $("#error").removeAttr("hidden");
+          $("#error").text(result.responseJSON.error);
+        }
       });
     } catch (e) {
       console.log(e);
     }
 });
-
-const isValidString = (
-    input,
-    name = "String input",
-    allow_empty = false
-  ) => {
-    if (!input) throw { status: 400, error: `${name} must be provided.` };
-    if (typeof input !== "string")
-      throw { status: 400, error: `${name} is not a string.` };
-    if (!allow_empty && input.trim().length === 0)
-      throw { status: 400, error: `${name} is blank.` };
-  
-    return input.trim();
-};
